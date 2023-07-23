133b9a86326bce8356a2e22e5957213402a62a2e
Ccolina03 <colinaperu2003@gmail.com>
2023-07-23 13:53:56 -0600

    Adding last CRUD operation in abstract repo

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,9 +1,9 @@
 // IDEA: common repo from which other microservices can extend from and use common CRUD operations from our database
 import { Logger, NotFoundException } from "@nestjs/common";
 import { AbstractDocument } from "./abstract.schema";
-import { Model, Types, FilterQuery} from "mongoose";
-import { filter } from "rxjs";
+import { Model, Types, FilterQuery, UpdateQuery} from "mongoose";
 
+//Key point: MongoDB knows method does certain thing from name of methods called
 export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     //nestjs js commong logger to make repo create to use CRUD methods
     //protected which allows sub repo classes to have access to them and allow any custom functionality 
@@ -31,7 +31,7 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     //filterQuery = query criteria for finding the document
     async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
         //lean= return document as plain JS object rather than Mongoose. No Hydration
-        const document = await this.model.findOne(filterQuery, {}, {lean: true}); //fields not specified
+        const document = await this.model.findOne(filterQuery, {}, {lean: true}); //fields not specified so entire document returned 
 
         if (!document) {
             this.logger.warn("Document was not found with", filterQuery);
@@ -40,4 +40,33 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
         return document; //return document if found
     }
     
+    //Not return anything void.
+    async findOneAndUpdate(
+        filterQuery: FilterQuery<TDocument>,
+        update: UpdateQuery<TDocument>,
+    ){
+        const document = await this.model.findOneAndUpdate(filterQuery, update, {
+            lean: true,  //unhydrated document
+            new: true, //to return the newly updated document not the old one
+        })
+
+        if (!document) {
+            this.logger.warn("Document was not found with", filterQuery);
+            throw new NotFoundException("Document not Found") //HTTP STATUS CODE SET TO 404 automatically
+        }
+        return document; //return document if found
+    }
+
+
+    //returns array of TDocuments 
+    //Goal: now when using find method on extended microservice it will receive an array of TDocument objects that match given query criteria
+    async find (filterQuery: FilterQuery<TDocument>) {
+        return this.model.find(filterQuery, {}, {lean: true} );
+    }
+
+    //returns null if not found. If not returns Document deleted
+    async findOneAndDelete (filterQuery: FilterQuery<TDocument>) {
+        return this.model.findOneAndDelete(filterQuery, {lean:true} )
+    }
+
 }
