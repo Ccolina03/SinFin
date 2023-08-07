bd4a8db635a60f78fc4696d5c1adf0a09239a659
Ccolina03 <colinaperu2003@gmail.com>
2023-08-06 21:20:16 -0600

    Implemented UsersService and setting up UsersRepository to employ Abstract Repository methods

---
+++ apps/auth/src/users/users.repository.ts
@@ -0,0 +1,7 @@
+import { AbstractRepository } from '@app/common';
+import {Injectable} from '@nestjs/common'
+
+@Injectable()
+export class UsersRepository  {
+
+}
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly UsersRepository: UsersRepository) {} 
+
+    create(createUserDTO: CreateUserDto) {
+        return this.UsersRepository.create(createUserDTO);
+    }
+}
